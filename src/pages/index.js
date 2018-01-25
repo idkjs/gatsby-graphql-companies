fb5792f9797e5a89b89b74c2b8ef60d25de6f1d8
Alain Armand <aarmand.inbox@gmail.com>
2018-01-25 23:39:01 +0100

    styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,43 @@
-import React from "react"
+import React from "react";
+import styles from "./about-css-modules.module.css";
+import Container from "../components/container";
 
-export default ({data}) => {
-	return(
-		<div>
-			<h1>Basic Hiragana</h1>
-			<table>
-			    <thead>
-			        <tr>
-			            <th>Hiragana</th>
-			            <th>Romanji</th>
-			        </tr>
-			    </thead>
-			      	<tbody>
-			          {data.allHiraganaJson.edges.map(({ node }) =>
-			            <tr>
-			              <td>
-			                {node.hiragana}
-			              </td>
-			              <td>
-			                {node.romanji}
-			              </td>
-			            </tr>
-			          )}
-			    	</tbody>
-			</table>
-		</div>
-		)
-}
+const Company = props => (
+  <div className={styles.company}>
+    <img src={props.logo} className={styles.logo} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.name}>{props.name}</h2>
+      <p className={styles.excerpt}>{props.description}</p>
+    </div>
+  </div>
+);
+
+export default ({ data }) => {
+  return (
+    <Container>
+      <h1>LaFrenchTech</h1>
+      {data.allSourcesJson.edges.map(({ node }) => (
+        <Company
+          name={node.name}
+          description={node.description}
+          logo={node.logo}
+        />
+      ))}
+    </Container>
+  );
+};
 
 export const query = graphql`
-  query MyFilesQuery {
-   allHiraganaJson {
-    edges {
-      node {
-        hiragana
-        romanji
-     	}
+  query MyCoQuery {
+    allSourcesJson {
+      edges {
+        node {
+          name
+          description
+          logo
+          url
+        }
       }
-  	}
+    }
   }
-`
+`;
